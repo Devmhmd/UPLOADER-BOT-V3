929635a91072a371e38058695f41a78dc7e8d5d1
المطور فيرس <104868036+Devmhmd@users.noreply.github.com>
2023-02-25 17:11:18 +0200

    Update help_uploadbot.py

--- functions/help_uploadbot.py
+++ functions/help_uploadbot.py
@@ -8,7 +8,7 @@ import os
 import requests
 
 def DetectFileSize(url):
-    r = requests.get(url, allow_redirects=True, stream=True)
+    r = requests.get(url, verify=False, allow_redirects=True, stream=True)
     total_size = int(r.headers.get("content-length", 0))
     return total_size
 
@@ -18,7 +18,7 @@ def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_i
         os.remove(file_name)
     if not url:
         return file_name
-    r = requests.get(url, allow_redirects=True, stream=True)
+    r = requests.get(url, verify=False, allow_redirects=True, stream=True)
     # Support Group Join @NT_BOTS_SUPPORT
     total_size = int(r.headers.get("content-length", 0))
     downloaded_size = 0
